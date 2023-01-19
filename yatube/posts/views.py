4488e3c8fbdde87fee7d50aef2798f564bdb17d3
Андрей Моисеев <vir2ozz2009@gmail.com>
2023-01-19 20:19:51 +0300

    версия для тестов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
 
-# Create your views here.
-# Главная страница
+
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template) 
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Главная страница'
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
-# Страница с информацией об одном сорте мороженого;
-# view-функция принимает параметр slug из path()
 def group_posts(request, slug):
-    return HttpResponse(f'Группа с названием <b>{slug}</b>') 
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    title = f'Группа {slug}'
+    text = f'Страница группы {slug}'
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+        'text': text
+    }
+    return render(request, template, context) 
